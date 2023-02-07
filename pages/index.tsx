5a9abac7d9ad8258f30a9638ac3168cc1962913d
yyy-muu <rirakkuma.0918.y@gmail.com>
2023-02-08 00:00:07 +0900

    Add toppage styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,73 @@ import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import utilStyles from "../styles/utils.module.css"
 
 
 export default function Home() {
   return (
-    <Layout></Layout>
+    <Layout>
+      <section className={utilStyles.headingMd}>
+        <p>
+          駆け出しエンジニアです
+        </p>
+      </section>
+
+      <section>
+        <h2>ブログ</h2>
+        <div className={styles.grid}>
+          <article>
+            <Link href='/'>
+              <img src="/images/thumbnail01.jpg" className={styles.thumbnailImage}/>
+            </Link>
+            <Link href='/' className={utilStyles.boldText}>
+              SSGとSSRの使いわけ
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              2023/2/7
+            </small>
+          </article>
+          <article>
+            <Link href='/'>
+              <img src="/images/thumbnail01.jpg" className={styles.thumbnailImage}/>
+            </Link>
+            <Link href='/' className={utilStyles.boldText}>
+              SSGとSSRの使いわけ
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              2023/2/7
+            </small>
+          </article>
+          <article>
+            <Link href='/'>
+              <img src="/images/thumbnail01.jpg" className={styles.thumbnailImage}/>
+            </Link>
+            <Link href='/' className={utilStyles.boldText}>
+              SSGとSSRの使いわけ
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              2023/2/7
+            </small>
+          </article>
+          <article>
+            <Link href='/'>
+              <img src="/images/thumbnail01.jpg" className={styles.thumbnailImage}/>
+            </Link>
+            <Link href='/' className={utilStyles.boldText}>
+              SSGとSSRの使いわけ
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              2023/2/7
+            </small>
+          </article>
+        </div>
+      </section>
+
+
+    </Layout>
   );
 }
