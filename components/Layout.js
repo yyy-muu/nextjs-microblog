e80fb2c17528e6d3ab993432dad422228824ac6c
yyy-muu <rirakkuma.0918.y@gmail.com>
2023-02-07 23:40:18 +0900

    Add profilepage layouts

---
+++ components/Layout.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import styles from "./layout.module.css"
+import utilStyles from "../styles/utils.module.css"
+
+const name = "yui"
+export const siteTitle = "Next.js blog"
+
+export default function Layout({children}) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <img src="/images/profile.png" className={utilStyles.borderCircle}/>
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+}
