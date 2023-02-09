e4aa426a6a88f700013bde10a6d763d5bf5d13c5
yyy-muu <rirakkuma.0918.y@gmail.com>
2023-02-10 00:15:43 +0900

    Add routing settings

---
+++ pages/posts/[id].js
@@ -0,0 +1,34 @@
+import Layout from '../../components/Layout'
+import {getAllPostIds, getPostData} from '../../lib/post'
+import utilStyles from "../../styles/utils.module.css"
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+
+  return {
+    paths,
+    fallback: false //取得path以外は404ページを返す
+  }
+}
+
+export async function getStaticProps({params}) {
+  const postData = await getPostData(params.id)
+
+  return {
+    props: {
+      postData,
+    }
+  }
+}
+
+export default function Post({postData}) {
+  return (
+    <Layout>
+      <article>
+        <h1 className={utilStyles.headingX1}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{__html: postData.blogContentHTML}}/>
+      </article>
+    </Layout>
+  );
+}
