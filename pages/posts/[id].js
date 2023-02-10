35880ff11bd8c7bf260b27e5b5ab0b0903395b3f
yyy-muu <rirakkuma.0918.y@gmail.com>
2023-02-10 23:36:04 +0900

    Add & Fix page layout

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Layout from '../../components/Layout'
 import {getAllPostIds, getPostData} from '../../lib/post'
 import utilStyles from "../../styles/utils.module.css"
@@ -24,6 +25,9 @@ export async function getStaticProps({params}) {
 export default function Post({postData}) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       <article>
         <h1 className={utilStyles.headingX1}>{postData.title}</h1>
         <div className={utilStyles.lightText}>{postData.date}</div>
